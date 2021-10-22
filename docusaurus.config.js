613938223f7e38f7656f43fe0489e4dc124f3250
coprocoder <mog.ie@mail.ru>
2021-10-22 15:46:54 +0700

    security + header pins

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,6 +35,7 @@ const config = {
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
+          // customCss1: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
