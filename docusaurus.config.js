bbd32741d3c830345df37b8ce7e30ed4d3bdeee1
coprocoder <mog.ie@mail.ru>
2022-04-21 03:35:49 +0700

    Rm unused md

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,17 +51,7 @@ const config = {
       {
         // `hashed` is recommended as long-term-cache of index file is possible.
         hashed: true,
-        language: ["en", "ru", "fr", "zh"],
-        translations: {
-          search_placeholder: "Search",
-          see_all_results: "See all results",
-          no_results: "No results.",
-          search_results_for: 'Search results for "{{ keyword }}"',
-          search_the_documentation: "Search the documentation",
-          count_documents_found: "{{ count }} document found",
-          count_documents_found_plural: "{{ count }} documents found",
-          no_documents_were_found: "No documents were found",
-        },
+        language: ["ru", "en", "fr", "zh"],
       },
     ],
   ],
